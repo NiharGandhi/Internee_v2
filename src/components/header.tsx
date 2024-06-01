659ab700af90ecfb6f4447fee179171542144864
Nihar Gandhi <nihargandhi93@gmail.com>
2024-06-01 18:10:43 +0400

    PATCH - Header Element

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,29 +4,16 @@ import React from 'react'
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
 import ShimmerButton from './magicui/shimmer-button'
-import { hasSubscription } from '@/lib/stripe'
-
 
 const Header = async () => {
 
-    const hasSubscribed = await hasSubscription();
+    // const hasSubscribed = await hasSubscription();
     
     return (
         <div className='bg-[#6c5ce7] dark:border-gray-800 dark:bg-gray-950 flex items-center justify-between py-2 px-4'>
-            {hasSubscribed ? (
-                <>
-                    <Link href="/">
-                        <h1 className='font-bold text-white text-4xl font-sans'>Internee <span className='text-gradient'>Pro</span></h1>
-                    </Link>
-                </>
-            ) : (
-                <>
-                    <Link href="/">
-                        <h1 className='font-bold text-white text-4xl font-sans'>Internee</h1>
-                    </Link>
-                </>
-            )}
-
+            <Link href="/">
+                <h1 className='font-bold text-white text-4xl font-sans'>Internee</h1>
+            </Link>
             <div className='flex items-center ml-auto'>
                 <SignedOut>
                     <SignInButton mode="modal">
